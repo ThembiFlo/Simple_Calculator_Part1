33c76a34b55422982fb97976de789fabb965ebfa
ThembiShabalala <thembi.shabalala@umuzi.org>
2019-06-14 15:32:46 +0200

    first commit

---
+++ calculator.js
@@ -0,0 +1,16 @@
+function add(){
+        let result = 0;
+        for(let i =0; i < arguments.length; i++){
+        result += arguments[i];
+    }    
+
+    return result;
+}
+
+function multiply(){
+        let result = 1;
+        for(let i = 0; i < arguments.length; i++){
+        result *= arguments[i];
+    }
+    return result;
+}
