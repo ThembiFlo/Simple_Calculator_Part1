4e352a9dffc5f8e2d0004461e1c6abf3e4b67776
ThembiShabalala <thembi.shabalala@umuzi.org>
2019-09-19 09:48:44 +0200

    tidy code

--- calculator.js
+++ calculator.js
@@ -1,16 +1,15 @@
-function add(){
-        let result = 0;
-        for(let i =0; i < arguments.length; i++){
-        result += arguments[i];
-    }    
-
-    return result;
+function add() {
+	let result = 0;
+	for (let i = 0; i < arguments.length; i++) {
+		result += arguments[i];
+	}
+	return result;
 }
 
-function multiply(){
-        let result = 1;
-        for(let i = 0; i < arguments.length; i++){
-        result *= arguments[i];
-    }
-    return result;
+function multiply() {
+	let result = 1;
+	for (let i = 0; i < arguments.length; i++) {
+		result *= arguments[i];
+	}
+	return result;
 }
--- calculatorSpec.js
+++ calculatorSpec.js
@@ -1,20 +1,21 @@
-describe("Calculator", function(){
-    it ("should add two numbers", function(){
-        expect(add(1,2)).toBe(3);
-    });
+describe('Calculator', function() {
+	it('should add two numbers', function() {
+		expect(add(1, 2)).toBe(3);
+	});
 
-    it("should add negative numbers ", function(){
-        expect(add(-1,-2)).toBe(-3);
-     
-     });
+	it('should add negative numbers ', function() {
+		expect(add(-1, -2)).toBe(-3);
+	});
 
-   it("should multiply two numbers", function(){
-       expect(multiply(1,2)).toBe(2);
-   });
-   it("should add multiple numbers", function(){
-       expect(add(1,2,3,4)).toBe(10);
-   });
-   it("should multiply multiple numbers", function(){
-       expect(multiply(1,2,3,4)).toBe(24);
-   })
-})
+	it('should add multiple numbers', function() {
+		expect(add(1, 2, 3, 4)).toBe(10);
+	});
+
+	it('should multiply two numbers', function() {
+		expect(multiply(1, 2)).toBe(2);
+	});
+
+	it('should multiply multiple numbers', function() {
+		expect(multiply(1, 2, 3, 4)).toBe(24);
+	});
+});
